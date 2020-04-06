e346c9773c7be58bb75cd39584933c368d58ca4d
Bykvoeshka <57699980+Bykvoeshka@users.noreply.github.com>
2020-04-06 16:45:26 +0300

    Update task.js

---
+++ element-search/menu/task.js
@@ -0,0 +1,28 @@
+  
+let mainMenuItems = document.querySelectorAll("ul.menu_main>li");
+let subMenuOpenFlag = 0;
+
+function openSubmenu() {
+    let subMenuElement = this.getElementsByClassName("menu menu_sub");
+
+    if (subMenuElement) {
+        if (subMenuElement[0].className == "menu menu_sub menu_active") {
+            subMenuElement[0].className = "menu menu_sub";
+            subMenuOpenFlag = 0;
+            return false;
+        }
+
+        if (subMenuElement[0].className == "menu menu_sub") {
+            if (subMenuOpenFlag != 0) {
+                subMenuOpenFlag.className = "menu menu_sub";
+            }
+            subMenuElement[0].className = "menu menu_sub menu_active";
+            subMenuOpenFlag = subMenuElement[0];
+            return false;
+        }
+    }
+}
+
+for (const itms of mainMenuItems) {
+    itms.onclick = openSubmenu;
+}
